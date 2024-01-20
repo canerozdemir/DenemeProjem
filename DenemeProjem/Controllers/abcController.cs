fb39071e6fc252e8e7a2c40600c09ade588b0ddf
Şevval Yıldırım <ysevvalyldrm@gmail.com>
2024-01-20 12:54:33 +0300

    controller ekledim

---
+++ DenemeProjem/Controllers/abcController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DenemeProjem.Controllers
+{
+	public class abcController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
