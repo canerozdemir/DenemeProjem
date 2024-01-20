b19c04612c1778a3d077e4167329c1ac71d9d52c
o-ozkaya <ozkaya19ogun@gmail.com>
2024-01-20 13:29:43 +0300

    Commented.

---
+++ DenemeProjem/Controllers/CommentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DenemeProjem.Controllers
+{
+    public class CommentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Only commenting here.
+    }
+}
